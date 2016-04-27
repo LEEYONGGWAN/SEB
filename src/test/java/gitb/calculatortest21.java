c9703ef4fc3014b6a9048bc37dab3f67c2b3e6fa
pms <cjswotls2@naver.com>
2016-04-27 14:41:28 +0900

    add기능추가

---
+++ src/test/java/gitb/calculatortest21.java
@@ -0,0 +1,14 @@
+package gitb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calculatortest21 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
